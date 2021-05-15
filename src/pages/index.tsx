fcf74abef5b86b81a87eda9d309a291950935dee
Ryo Ando <sasigume@gmail.com>
2021-05-16 03:43:32 +0900

    fixed unused container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { Text } from "@chakra-ui/react";
 
 import { Hero } from "../components/Hero";
-import { Container } from "../components/Container";
 import { Main } from "../components/Main";
 import { DarkModeSwitch } from "../components/DarkModeSwitch";
 import { CTA } from "../components/CTA";
