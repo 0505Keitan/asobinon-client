77b81ed5ef888ef98c897628200b72788c873494
Ryo Ando <sasigume@gmail.com>
2021-05-19 20:00:13 +0900

    added function to notice NSFW image

---
+++ functions/src/models/userimage.d.ts
@@ -0,0 +1,9 @@
+import * as admin from 'firebase-admin';
+
+export interface UserImage {
+  alt: string | null;
+  src: string;
+  filename: string;
+  uploadedTimeStamp: admin.firestore.FieldValue;
+  nsfw: 0 | 1 | 2 | 3;
+}
