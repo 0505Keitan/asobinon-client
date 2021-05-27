36ab16c95da82177283789c90a9ceb8f1beab4bf
Ryo Ando <sasigume@gmail.com>
2021-05-27 20:45:52 +0900

    [firebase] removed image notice function

--- functions/src/contact/index.ts
+++ functions/src/contact/index.ts
@@ -1,5 +1,3 @@
 import contactWithMailjet from './contact-with-mailjet';
-import firestoreNotice from './firestore-notice';
 
 exports.contactWithMailjet = contactWithMailjet;
-exports.filestoreNotice = firestoreNotice;
