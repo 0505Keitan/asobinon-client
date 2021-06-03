78a2c96aefef8ffdc32a4cf51bf8de3b8e7e1fe6
Ryo Ando <sasigume@gmail.com>
2021-06-03 12:36:43 +0900

    [feedback] Added feedback list close #12

--- src/pages/feedback/index.tsx
+++ src/pages/feedback/index.tsx
@@ -1,51 +1,55 @@
-import { Badge, Box } from '@chakra-ui/react';
-import firebase from 'firebase/app';
+import LinkChakra from '@/components/common/link-chakra';
+import { Badge, Box, Button, Stack } from '@chakra-ui/react';
 import { GetServerSidePropsContext } from 'next';
 import Layout from '../../components/layout';
 
 interface PageProps {
   path?: string;
   value?: 'good' | 'bad';
+  error?: string;
 }
 
-const FeedBackPage = ({ path, value }: PageProps) => {
-  const SendFeedback = async () => {
-    // pathはスラッシュがあるので、そのまま名前にするとサブコレクションとして解釈される
-    if (path) {
-      const docName = path.replace(/\//g, '_');
-      const docRef = firebase.firestore().collection('feedbacks').doc(docName);
-      if (value == 'good') {
-        docRef
-          .set(
-            {
-              good: firebase.firestore.FieldValue.increment(1),
-            },
-            { merge: true },
-          )
-          .then(() => console.debug('Added good'));
-      }
-      if (value == 'bad') {
-        docRef
-          .set(
-            {
-              bad: firebase.firestore.FieldValue.increment(1),
-            },
-            { merge: true },
-          )
-          .then(() => console.debug('Added bad'));
-      }
-    }
-  };
+const FeedBackPage = ({ path, value, error }: PageProps) => {
+  if (typeof path === 'string') {
+    const actualPath =
+      process.env.DOCS_URL +
+      path
+        .replace('/docs', '')
+        .replace('index.md', '')
+        .replace(`.md`, '')
+        .replace(/[0-9]\-/g, '');
+    return (
+      <Layout meta={{ title: 'フィードバックありがとうございました', desc: '' }}>
+        <Stack spacing={6}>
+          {error ? (
+            <>
+              <Box>エラー: {error}</Box>
+            </>
+          ) : (
+            <>
+              <Box>フィードバックありがとうございました。</Box>
 
-  SendFeedback().catch((e) => console.error(e));
+              <Box bg="gray.100" p={6} rounded="xl">
+                評価したページ:
+                <LinkChakra isExternal href={actualPath}>
+                  <Badge textTransform="none">{actualPath}</Badge>
+                </LinkChakra>
+              </Box>
 
-  return (
-    <Layout meta={{ title: 'フィードバックありがとうございました', desc: '' }}>
-      <Box mb={6}>フィードバックありがとうございました。</Box>
-
-      <Badge>評価したページ: {path}</Badge>
-    </Layout>
-  );
+              {value == 'bad' && (
+                <Box>もし間違いがある場合は、編集ボタンから記事の修正もご検討ください。</Box>
+              )}
+            </>
+          )}
+          <Button as={LinkChakra} href="/feedback/list">
+            フィードバック一覧へ
+          </Button>
+        </Stack>
+      </Layout>
+    );
+  } else {
+    return;
+  }
 };
 
 export default FeedBackPage;
@@ -55,12 +59,40 @@ export const getServerSideProps = async ({ query }: GetServerSidePropsContext) =
   https://github.com/sasigume/asobinon/blob/main/website/src/components/docpage/rating.tsx
   フィードバックボタンから、pathとvalueが渡される
   */
-  const path = query.path;
-  const value = query.value;
-  return {
-    props: {
-      path: path ?? null,
-      value: value ?? null,
+  const path = query.path as string | undefined;
+  const value = query.value as string | undefined;
+
+  let api = `${process.env.FUNCTIONS_URL}/feedback-addFeedback?value=${value}`;
+  if (typeof path === 'string') {
+    api = api + `&path=${encodeURIComponent(path)}`;
+  }
+  return await fetch(api, {
+    method: 'POST',
+    headers: {
+      Authorization: process.env.FUNCTIONS_AUTH ?? '',
     },
-  };
+  })
+    .then(async (res) => {
+      if (res.ok) {
+        return {
+          props: {
+            path,
+            value,
+          },
+        };
+      } else {
+        return {
+          props: {
+            path,
+            value,
+            error: `${res.statusText}`,
+          },
+        };
+      }
+    })
+    .catch((e) => {
+      return {
+        error: e.message ?? JSON.stringify(e),
+      };
+    });
 };
